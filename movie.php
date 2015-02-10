738c39304b25fe541b78967f8f30227beea37294
Notgordon <studentguac@gmail.com>
2015-02-09 21:51:02 -0800

    variables prepared

--- movie.php
+++ movie.php
@@ -19,13 +19,13 @@ if (!$mysqli->query("DROP TABLE IF EXISTS vidstore") ||
 !$mysqli->query("CREATE TABLE vidstore(id INT NOT NULL AUTO_INCREMENT, 
 name VARCHAR(255) UNIQUE NOT NULL, 
 category VARCHAR(255) NOT NULL,
-length INT,
+length INT NOT NULL,
 PRIMARY KEY( id ))") ||
 !$mysqli->query("INSERT INTO vidstore(id, name) VALUES (1, 'TEST')")) {
 	echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
-if (!($stmt = $mysqli->prepare("SELECT id, name FROM vidstore"))) {
+if (!($stmt = $mysqli->prepare("SELECT id, name, category, length FROM vidstore"))) {
     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
@@ -35,14 +35,16 @@ if (!$stmt->execute()) {
 
 $out_id = NULL;
 $out_name = NULL;
-
-if (!$stmt->bind_result($out_id, $out_name)) {
+$out_category = NULL;
+$out_length = NULL;
+if (!$stmt->bind_result($out_id, $out_name, $out_category, $out_length)) {
     echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 
 
+
 while ($stmt->fetch()) {
-    printf("id = %s (%s), name = %s (%s)\n", $out_id, gettype($out_id), $out_name, gettype($out_name));
+    printf("id = %s (%s), name = %s (%s), category = %s(%s) length = %s(%s)\n", $out_id, gettype($out_id), $out_name, gettype($out_name), $out_category, gettype($out_category), $out_length, gettype($out_length));
 }
 ?>
 </body>
